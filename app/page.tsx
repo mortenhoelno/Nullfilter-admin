3807152fdc38e5521001d6c8973d8ebf9ebe42a3
mortenhoelno <morten@mortenhoel.no>
2025-09-02 13:28:11 +0200

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Velkommen til Null Filter 👋</h1>
-      <p>Startside er oppe og går. Du kan nå navigere til <code>/admin</code> eller <code>/nystart</code>.</p>
-    </div>
-  )
-}
