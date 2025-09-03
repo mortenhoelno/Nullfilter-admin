3fd0cff33201599b28698ef61beee3cf17e4aaab
mortenhoelno <morten@mortenhoel.no>
2025-09-03 11:03:10 +0200

    Create supabaseClient.ts

---
+++ utils/supabaseClient.ts
@@ -0,0 +1,9 @@
+// utils/supabaseClient.ts
+import { createClient } from "@supabase/supabase-js";
+
+export const supabaseBrowser = () =>
+  createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, // ANON for klient
+    { auth: { persistSession: false } }
+  );
