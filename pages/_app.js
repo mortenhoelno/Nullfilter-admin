5e184125d6066e72ad26fcf5e9897d17a77df5b7
mortenhoelno <morten@mortenhoel.no>
2025-09-08 15:08:37 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,3 +4,16 @@ import "../styles/globals.css";
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+// pages/_app.js
+import ToastHost from "../components/ToastHost";
+import "../styles/globals.css"; // hvis du har globale styles
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastHost />
+    </>
+  );
+}
