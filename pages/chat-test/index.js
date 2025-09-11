298809832eb4fc705d958f9d7d740651d6936f1d
mortenhoelno <morten@mortenhoel.no>
2025-09-11 21:35:28 +0200

    Create index.js

---
+++ pages/chat-test/index.js
@@ -0,0 +1,14 @@
+import ChatEngineTest from "../../components/ChatEngineTest";
+
+export default function ChatTestPage() {
+  return (
+    <div className="h-screen flex flex-col">
+      <header className="p-4 bg-gray-800 text-white font-bold">
+        Testbot (SSE Prototype)
+      </header>
+      <main className="flex-1">
+        <ChatEngineTest />
+      </main>
+    </div>
+  );
+}
